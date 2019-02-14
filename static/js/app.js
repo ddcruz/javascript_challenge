94db76a897a81fbd0867ecf1cdde9292a8432574
deXta <ddcruz@email.com>
2019-02-13 23:26:39 -0600

    fixed comment

--- static/js/app.js
+++ static/js/app.js
@@ -3,23 +3,22 @@ var tbody = d3.select('#ufo-table>tbody');
 
 //function to display the data
 function display(thisData) {
+    //loop thru the data and insert each cell value per row
     thisData.forEach((x) => {
         tbody.append("tr");
         Object.entries(x).forEach(([key, value]) => {
             tbody.append("td").text(value);
         });
       });
-}
+};
 
 //function to clear the data
 function clearDisplay() {
     tbody.html("")
-}
+};
 
 //on page load, display all the data
 display(data);
-
-//loop thru the data and insert each cell value per row
 
 //get a handle on the submit button
 var submit = d3.select('#filter-btn');
